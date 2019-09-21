de6c0186b778f5a45146a5f6d868b495dde96f59
James Chiu <jachiu555@gmail.com>
2019-09-21 08:02:43 -0700

    Completed and passed all tests for ES6 class instatiation patterns.

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,21 @@
-class Bee {
+class Bee extends Grub {
   // TODO..
+  constructor() {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
+
+
 };
+
+
+/*
+the Grub superclass
+ an age property that is set to 5
+ a color property that is set to yellow
+ a food property that is inherited from grub
+ an eat method that is inherited from grub
+ a job property that is set to Keep on growing
+ */
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,29 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
   // TODO..
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = "make honey";
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    this.honeyPot += 1;
+  }
+
+  giveHoney() {
+    this.honeyPot -= 1;
+  }
 };
+
+/*
+the Bee superclass
+ an age property that is set to 10
+ a job property that is set to make honey
+ a color property inherited from bee that is set to yellow
+ a food property that is inherited from grub
+ an eat method that is inherited from grub
+ a honeyPot property that is set to 0
+ a makeHoney method that adds 1 to that honeyBee's honeyPot
+ a giveHoney method that subtracts 1 from that honeyBee's honeyPot
+ */
